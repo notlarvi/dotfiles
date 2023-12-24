007914aa48ab3fcb6dc83fbfff29c498e95a7105
notlarvi <larvipc@gmail.com>
2023-12-24 20:38:07 +0300

    Initial commit xD

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+-- Bootstrap lazy
+require "lazy-bootstrap"
+
+local lazy = require("lazy")
+lazy.setup("plugins")
+
+-- Settings
+require "set"
+require "keybindings"
+
+-- vim:tabstop=2 shiftwidth=2 expandtab syntax=lua foldmethod=marker foldlevelstart=0 foldlevel=0
