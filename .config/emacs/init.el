d7ee57bf79215a2bf666cf4f8e1ed4fdd96a7e86
notlarvi <larvipc@gmail.com>
2024-09-22 01:48:53 +0300

    Some emacs changes

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -8,10 +8,18 @@
  '(custom-safe-themes
    '("014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "f4d1b183465f2d29b7a2e9dbe87ccc20598e79738e5d29fc52ec8fb8c576fcfd" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default))
  '(package-selected-packages
-   '(org-modern org-bullets telega doom-modeline ivy evil doom-themes)))
+   '(denote org-modern org-bullets telega doom-modeline ivy evil doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#f6f3e8" :height 1.6 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#f6f3e8" :height 1.5))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#f6f3e8" :height 1.3))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#f6f3e8" :height 1.2))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#f6f3e8" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#f6f3e8"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#f6f3e8"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#f6f3e8"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#f6f3e8")))))
