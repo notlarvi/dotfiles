c45942f6d36bae5e96b9d9d114d7c9db6550fb0a
notlarvi <larvipc@gmail.com>
2024-09-22 20:58:55 +0300

    Emacssssss

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -8,7 +8,7 @@
  '(custom-safe-themes
    '("014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "f4d1b183465f2d29b7a2e9dbe87ccc20598e79738e5d29fc52ec8fb8c576fcfd" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default))
  '(package-selected-packages
-   '(denote org-modern org-bullets telega doom-modeline ivy evil doom-themes)))
+   '(counsel ivy-rich rainbow-delimiters org-modern org-bullets telega doom-modeline ivy evil doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
